46c21e93fde2a14b083ba2e1e10e63f517a5cd0f
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-07-23 23:48:54 -0400

    Update fork_join.sv

--- Day_2/fork_join.sv
+++ Day_2/fork_join.sv
@@ -1,1 +1,18 @@
-
+module tb;
+initial begin
+	$display("[%0t] main thread: Fork join is about to start", $time);
+	fork
+	#40 $display("[%0t]thread1finished",$time);
+	begin
+	
+		#2 $display("[%0t]thread2",$time);
+		#20 $display("[%0t]Thread2 finished",$time);
+	
+	end
+	
+	#30 $display("[%0t]Thread 3",$time);
+	join
+	$display("[%0t]thread1finished",$time);
+	
+end
+endmodule
