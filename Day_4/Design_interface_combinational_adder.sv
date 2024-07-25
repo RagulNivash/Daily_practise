706d5704dc84b00c9d708d167cd7a7735918ed31
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-07-25 09:18:10 -0400

    Create Design_interface_combinational_adder.sv

---
+++ Day_4/Design_interface_combinational_adder.sv
@@ -0,0 +1,14 @@
+moudle (input [3:0] a, input [3:0] b, output [4:0]y);
+
+assign y= a+b;
+
+endmodule
+
+//////////////////////////////////////////////////////////
+
+interface add_if();
+  logic [3:0] a;
+  logic [3:0] b;
+  logic [4:0] y;
+
+endinterface
