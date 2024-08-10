5024aa7b6e96e20f25881aa708e8b95b4ddb559c
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-08-09 23:55:32 -0400

    Update UVM_Multiplier_Seq.sv

--- Day_9/UVM_Multiplier_Seq.sv
+++ Day_9/UVM_Multiplier_Seq.sv
@@ -1,1 +1,29 @@
+module dff(input clk, input rst, input din, output reg dout);
 
+  always@(posedge clk)
+    begin
+      if(rst)
+        dout<=1'b0;
+      else
+        dout <=din;
+    end
+endmodule
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+interface dff_if;
+  logic clk;
+  logic rst;
+  logic din;
+  logic dout;
+endinterface
+
+//////////////////////////////////////////////////////////UVM_environment//////////////////////////////////////////////////////////////
+
+'inlude "uvm_macors.svh"
+import uvm_pkg::*;
+
+class congif_dff extends uvm_objec;]
+
+endclass
+  
