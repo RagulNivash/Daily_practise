60b4f448825a3e30d5b6e38c4358d4e01e9dde4a
RagulNivash <49038494+RagulNivash@users.noreply.github.com>
2024-08-18 23:30:51 -0400

    Update Practicing_c.cpp

--- Day_11/Practicing_c.cpp
+++ Day_11/Practicing_c.cpp
@@ -8,3 +8,29 @@ return 0;
 
 
 ///////////////////////////////////////////////////////
+#include <iostream>
+using namespace std;
+
+int main()
+{
+int age;
+  cout<< "enter your age"<<flush;
+  cin>> age;
+  cout<<"you"<<ageg<<"yea old"<<endl;
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
